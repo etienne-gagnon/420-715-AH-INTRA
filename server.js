c80e74c22fd8914081a711d677c75de67f68582e
Étienne Gagnon <etigag2002@gmail.com>
2025-01-07 14:45:14 -0500

    [INIT] - Création de la page index avec formulaire simple

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.sendFile(__dirname + '/index.html');
 })
+
+
 
 app.listen(7777);
 
