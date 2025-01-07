3a1010f8a1a1bec3ca1a939d35f350668e671558
Étienne Gagnon <etigag2002@gmail.com>
2025-01-07 14:55:44 -0500

    [UPD] - Update du formulaire + ajout des étapes dans test/livraison

--- test/livraison.test.js
+++ test/livraison.test.js
@@ -1,1 +1,19 @@
-// 
+// MEMBRE PREMIUM OU STANDARD
+
+// PREMIUM
+
+    // 0$ LA LIVRAISON
+
+
+// STANDARD 
+
+    // COÛT DE LA LIVRAISON
+
+        // SOUS TOTAL > 150$
+
+        // DISTANCES DE LIVRAISON
+
+    // AJOUT DE LA TAXE DE 2$
+
+
+
